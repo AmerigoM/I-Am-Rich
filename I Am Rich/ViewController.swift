b71253e22afbe21d6d889763400d96fea093c797
AmerigoM <amerigo.mancino@gmail.com>
2020-09-15 19:28:34 +0200

    First commit.

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Amerigo Mancino on 01/12/2018.
+//  Copyright © 2018 Amerigo Mancino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
